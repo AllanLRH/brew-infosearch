2f5ba687e029c989291b93a3e58d2d8937978498
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-03-29 09:55:49 +0200

    Get info from just tapped taps

--- brew-infosearch.py
+++ brew-infosearch.py
@@ -3,11 +3,15 @@
 
 import subprocess
 import sys
+import re
 
 # Check input arguments
 if len(sys.argv) > 2:
     print('Error! You must provide items to search for!', file=sys.stdout)
     sys.exit(1)
+
+tapRx = re.compile(r'[\w_-]+?/([\w_-]+)(?:/.)?')
+brewTapList = [tapRx.match(el).groups()[0] for el in subprocess.check_output('brew tap'.split()).decode('utf8').splitlines()]
 
 # Obtain search result, decode bytes type to str
 c = subprocess.Popen(('brew search ' + sys.argv[1]).split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -22,13 +26,19 @@ if stderr:
 
 # Call brew-info command if any search results were returned
 if stdout:
+    PRINTLINE = False
     for i, el in enumerate(stdout):
         print('')
-        if el.startswith('Caskroom/cask/'):
-            subprocess.call(('brew cask info ' + el).split())
+        m = tapRx.match(el)
+        if m:
+            prefix = m.groups()[0]
+            if prefix in brewTapList:
+                subprocess.call(['brew', prefix, 'info', el])
+                PRINTLINE = True
         else:
             subprocess.call(('brew info ' + el).split())
-        if i < len(stdout)-1:
+            PRINTLINE = True
+        if i < len(stdout)-2 and PRINTLINE:
             print('\n' + '≈'*79)
 else:
     print('No results found.')
